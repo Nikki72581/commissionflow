ec31cdf58d4afdf94721bdf65e0c7d88691e2c86
Nicole Ronchetti <nicoleronchetti@mac.com>
2025-12-01 20:33:51 -0500

    updated files

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,8 +5,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db = globalForPrisma.prisma ?? new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-});
+export const db = 
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
